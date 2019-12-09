f9c8138fbe61af7ffe09e9a28b4f5c0b0514edc9
Julien Pause <julien.pause@epitech.eu>
2019-12-09 22:30:35 +0400

    Add file_manipulation source file and its header file

---
+++ include/file_manipulation.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Header file for file manipulation
+*/
+
+#ifndef FILE_MANIPULATION_H_
+    #define FILE_MANIPULATION_H_
+
+    #define OPEN_FAILURE_MSG        "File cannot be opened\n"
+    #define READ_FAILURE_MSG        "File cannot be read\n"
+
+    char *get_file_buffer(char *filepath);
+    unsigned int get_file_size(char const *filepath);
+#endif
---
+++ src/file_manipulation.c
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2019
+** my_radar
+** File description:
+** Functions to open and read a file
+*/
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "file_manipulation.h"
+#include "my.h"
+
+char *get_file_buffer(char *filepath)
+{
+    int fd = 0;
+    unsigned int file_size = get_file_size(filepath);
+    char *buffer = malloc(sizeof(char) * (file_size + 1));
+
+    if ((fd = open(filepath, O_RDONLY)) == -1) {
+        my_puterr(OPEN_FAILURE_MSG);
+        return (NULL);
+    }
+    if (read(fd, buffer, file_size) == -1) {
+        my_puterr(READ_FAILURE_MSG);
+        return (NULL);
+    }
+    buffer[file_size] = '\0';
+    close(fd);
+    return (buffer);
+}
+
+unsigned int get_file_size(char const *filepath)
+{
+    struct stat stats;
+
+    if (stat(filepath, &stats) != 0)
+        return (-1);
+    return (stats.st_size);
+}
