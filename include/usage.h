4c6b0491ee2bac91b515c215adffa4ef06b5b3d2
Julien Pause <julien.pause@epitech.eu>
2019-12-10 23:08:25 +0400

    Add usage.c and its usage

---
+++ include/usage.h
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2019
+** my_radar
+** File description:
+** Header file for usage
+*/
+
+#ifndef USAGE_H_
+    #define USAGE_H_
+
+    #define MY_EXIT_SUCCESS         0
+    #define MY_EXIT_OPTION          192
+    #define MY_EXIT_FAILURE         84
+
+    #define SHORT_PROGRAM_DESCRIPTION                                          \
+    "Air traffic simulation panel\n"
+    #define USAGE                                                              \
+    "USAGE\n"                                                                  \
+    "  ./my_radar [OPTIONS] path_to_script\n"                                  \
+    "    path_to_script    The path to the script file.\n"
+    #define OPTIONS                                                            \
+    "OPTIONS\n"                                                                \
+    " -h | --help          print the usage and quit.\n"
+
+    int check_args(int ac, char **av);
+    int check_options(char *arg);
+    void print_help(void);
+    void print_bad_ac(int ac, char **av);
+#endif
---
+++ src/usage.c
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2019
+** my_radar
+** File description:
+** Handles command line arguments
+*/
+
+#include "usage.h"
+#include "my.h"
+
+int check_args(int ac, char **av)
+{
+    if (ac != 2) {
+        print_bad_ac(ac, av);
+        return (MY_EXIT_FAILURE);
+    }
+    if (check_options(av[1]) == MY_EXIT_SUCCESS)
+        return (MY_EXIT_OPTION);
+    return (MY_EXIT_SUCCESS);
+}
+
+int check_options(char *arg)
+{
+    if (my_strcmp(arg, "-h") == 0 || my_strcmp(arg, "--help") == 0) {
+        print_help();
+        return (MY_EXIT_SUCCESS);
+    }
+    return (MY_EXIT_FAILURE);
+}
+
+void print_help(void)
+{
+    my_putstr(SHORT_PROGRAM_DESCRIPTION);
+    my_putchar('\n');
+    my_putstr(USAGE);
+    my_putchar('\n');
+    my_putstr(OPTIONS);
+}
+
+void print_bad_ac(int ac, char **av)
+{
+    my_puterr(av[0]);
+    my_puterr(": bad arguments: ");
+    my_puterr(my_int_to_strnum(ac - 1));
+    my_puterr(" given but 1 is required\n");
+    my_puterr("retry with -h\n");
+}
