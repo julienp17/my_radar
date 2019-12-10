5036a863e8dd0e43dd68c59694d75e379537c0d6
Julien Pause <julien.pause@epitech.eu>
2019-12-10 22:57:47 +0400

    Add my_putstr to libmy

--- lib/my/my.h
+++ lib/my/my.h
@@ -8,6 +8,7 @@
 #ifndef MY_H_
     #define MY_H_
 
+    void my_putstr(char const *str);
     void my_puterr(char const *str);
 
     unsigned int my_count_char(char const *str, char const to_count);
