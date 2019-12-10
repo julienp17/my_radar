a6f438c11cc2a4d48b2a84d3d67783474b7bad64
Julien Pause <julien.pause@epitech.eu>
2019-12-10 23:09:01 +0400

    Call usage in main

--- src/main.c
+++ src/main.c
@@ -5,7 +5,16 @@
 ** Main file for my_radar
 */
 
-int main(void)
+#include "usage.h"
+
+int main(int ac, char **av)
 {
-    return (0);
+    int error_code = 0;
+
+    error_code = check_args(ac, av);
+    if (error_code == MY_EXIT_OPTION)
+        return (MY_EXIT_SUCCESS);
+    else if (error_code == MY_EXIT_FAILURE)
+        return (error_code);
+    return (MY_EXIT_SUCCESS);
 }
