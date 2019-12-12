79c8f88e915170365e54a420ad9a9ca921236582
Julien Pause <julien.pause@epitech.eu>
2019-12-12 17:20:39 +0400

    Remove temporary comments in my_radar.c

--- src/my_radar.c
+++ src/my_radar.c
@@ -14,9 +14,7 @@
 #include "plane.h"
 #include "tower.h"
 
-// void draw_towers(sfRenderWindow *window, tower_t **towers);
-// static void init_towers(sfTexture **texture, sfSprite **sprite,
-//                         sfCircleShape **control_area);
+void draw_towers(sfRenderWindow *window, tower_t **towers);
 
 int my_radar(char const *file_path)
 {
@@ -52,6 +50,7 @@ void simulation_loop(window_t *window, plane_t **planes, tower_t **towers)
         sfRenderWindow_clear(window->window, sfWhite);
         sfRenderWindow_drawSprite(window->window, window->background_sprite,
                                     NULL);
+        draw_towers(window->window, towers);
         sfRenderWindow_display(window->window);
     }
 }
@@ -74,44 +73,5 @@ void destroy_all(window_t *window, plane_t **planes, tower_t **towers)
     for (unsigned int i = 0 ; planes[i] ; i++)
         free(planes[i]);
     for (unsigned int i = 0 ; towers[i] ; i++)
-        free(towers[i]);
+        tower_destroy(towers[i]);
 }
-
-// void draw_towers(sfRenderWindow *window, tower_t **towers)
-// {
-//     static bool first_run = true;
-//     static sfTexture *tower_texture = NULL;
-//     static sfSprite *tower_sprite = NULL;
-//     static sfCircleShape *control_area = NULL;
-//     sfVector2f tower_pos;
-
-//     if (first_run) {
-//         init_towers(&tower_texture, &tower_sprite, &control_area);
-//         first_run = false;
-//     }
-//     for (unsigned int i = 0 ; towers[i] ; i++) {
-//         tower_pos.x = towers[i]->pos.x;
-//         tower_pos.y = towers[i]->pos.y;
-//         if (!tower_sprite)
-//             printf("tower_sprite is NUll\n");
-//         sfSprite_setPosition(tower_sprite, tower_pos);
-//         sfCircleShape_setRadius(control_area, towers[i]->radius);
-//         sfCircleShape_setPosition(control_area, tower_pos);
-//         sfRenderWindow_drawSprite(window, tower_sprite, NULL);
-//         sfRenderWindow_drawCircleShape(window, control_area, NULL);
-//     }
-// }
-
-// static void init_towers(sfTexture **texture, sfSprite **sprite,
-//                         sfCircleShape **control_area)
-// {
-//     sfIntRect pos = {0, 0, 1366, 768};
-
-//     *texture = sfTexture_createFromFile("assets/tower_small.png", &pos);
-//     *sprite = sfSprite_create();
-//     *control_area = sfCircleShape_create();
-//     sfSprite_setTexture(*sprite, *texture, sfTrue);
-//     sfCircleShape_setFillColor(*control_area, sfTransparent);
-//     sfCircleShape_setOutlineColor(*control_area, sfBlue);
-//     sfCircleShape_setOutlineThickness(*control_area, 2);
-// }
