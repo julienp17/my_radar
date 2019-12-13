2fde813b8991e58924c9dfab18829e6eeefa14e2
Julien Pause <julien.pause@epitech.eu>
2019-12-13 10:40:52 +0400

    Add draw header file

---
+++ include/draw.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** my_radar
+** File description:
+** Header file for drawing to the screen
+*/
+
+#ifndef DRAW_H_
+    #define DRAW_H_
+
+    #include "plane.h"
+    #include "tower.h"
+
+    void draw_towers(sfRenderWindow *window, tower_t **towers);
+    void draw_planes(sfRenderWindow *window, plane_t **planes);
+#endif
