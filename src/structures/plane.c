81e66ea2ad2112bd73dc3176b868cf3e42e36382
Julien Pause <julien.pause@epitech.eu>
2019-12-14 23:41:29 +0400

    Rotate planes based on their path

--- src/structures/plane.c
+++ src/structures/plane.c
@@ -5,7 +5,9 @@
 ** Source file for airplane structure
 */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <SFML/Graphics.h>
 #include "plane.h"
 
@@ -16,7 +18,6 @@ plane_t *plane_create(sfVector2f departure, sfVector2f arrival,
 
     plane->pos       = departure;
     plane->end       = arrival;
-    plane->rect      = (sfIntRect) {departure.x, departure.y, 20, 20};
     plane->speed     = speed;
     plane->delay     = delay;
     plane->step      = get_step_offset(departure, arrival);
@@ -26,6 +27,8 @@ plane_t *plane_create(sfVector2f departure, sfVector2f arrival,
     plane->sprite    = sfSprite_create();
     sfSprite_setTexture(plane->sprite, plane->texture, sfTrue);
     sfSprite_setPosition(plane->sprite, plane->pos);
+    sfSprite_rotate(plane->sprite,
+                    get_angle_from_coordinate(arrival, departure));
     return (plane);
 }
 
@@ -51,4 +54,17 @@ sfVector2f get_step_offset(sfVector2f point_a, sfVector2f point_b)
     offset.x = dx / (float)steps;
     offset.y = dy / (float)steps;
     return (offset);
+}
+
+float get_angle_from_coordinate(sfVector2f point_a, sfVector2f point_b)
+{
+    float dx = point_b.x - point_a.x;
+    float dy = point_b.y - point_a.y;
+    float radians = atan2f(dx, dy);
+
+    if (radians < 0.0)
+        radians = fabs(radians);
+    else
+        radians = 2 * M_PI - radians;
+    return (180.0 / M_PI * radians);
 }
