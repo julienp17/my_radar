67f1b7dd84e736d402e69eb20b73035ca760cf83
Julien Pause <julien.pause@epitech.eu>
2019-12-27 17:24:19 +0400

    Refactor plane header file with new attribute struct path

--- include/structures/plane.h
+++ include/structures/plane.h
@@ -9,23 +9,18 @@
     #define AIRPLANE_H_
 
     #include <SFML/Graphics.h>
+    #include "path.h"
 
     #define PLANE_SYMBOL            'A'
     #define PLANE_TEXTURE_PATH      "assets/textures/plane2_small.png"
 
     typedef struct plane {
-        sfVector2f pos;
-        sfVector2f end;
-        unsigned int speed;
-        unsigned int delay;
-        sfVector2f step;
-        sfTexture *texture;
+        path_t *path;
         sfSprite *sprite;
+        unsigned int delay;
     } plane_t;
 
-    plane_t *plane_create(sfVector2f departure, sfVector2f arrival,
-                    unsigned int speed, unsigned int delay);
+    plane_t *plane_create(path_t *path, sfTexture *texture, unsigned int delay);
     void plane_update_pos(plane_t *plane);
-    sfVector2f get_step_offset(sfVector2f point_a, sfVector2f point_b);
     float get_angle_from_coordinate(sfVector2f point_a, sfVector2f point_b);
 #endif
