b36582be7b4aca2275f5643da2f55b2daabb2e3e
Julien Pause <julien.pause@epitech.eu>
2019-12-27 18:19:06 +0400

    Add sim.c

---
+++ src/structures/sim.c
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2019
+** my_radar
+** File description:
+** Source file for simulation structure
+*/
+
+#include <stdlib.h>
+#include "sim.h"
+#include "file_manipulation.h"
+#include "window.h"
+#include "plane.h"
+#include "tower.h"
+
+sim_t *sim_create_from_script(char const *filepath)
+{
+    sim_t *sim = malloc(sizeof(sim_t));
+
+    if (!sim)
+        return (NULL);
+    sim->window = window_create(W_WIDTH, W_HEIGHT, W_TITLE, SIM_BG_IMG_PATH);
+    sim->plane_texture = sfTexture_createFromFile(SIM_PLANE_TEXTURE_PATH, NULL);
+    sim->tower_texture = sfTexture_createFromFile(SIM_TOWER_TEXTURE_PATH, NULL);
+    sim->planes = NULL;
+    sim->towers = NULL;
+    if (!(sim->window) || !(sim->plane_texture) || !(sim->tower_texture))
+        return (NULL);
+    if (get_entities_from_file(filepath, sim) == -1)
+        return (NULL);
+    return (sim);
+}
+
+void sim_destroy(sim_t *sim)
+{
+    if (sim->window)
+        window_destroy(sim->window);
+    for (unsigned int i = 0 ; sim->towers[i] ; i++)
+        tower_destroy(sim->towers[i]);
+    for (unsigned int i = 0 ; sim->planes[i] ; i++)
+        plane_destroy(sim->planes[i]);
+    if (sim->plane_texture)
+        sfTexture_destroy(sim->plane_texture);
+    if (sim->tower_texture)
+        sfTexture_destroy(sim->tower_texture);
+    if (sim)
+        free(sim);
+}
