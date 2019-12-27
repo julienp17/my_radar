6302624887d566ef1907e1736be320e1aae12559
Julien Pause <julien.pause@epitech.eu>
2019-12-27 17:23:29 +0400

    Refactor plane_create and plane_update_pos functions, and create plane_destroy function

--- src/structures/plane.c
+++ src/structures/plane.c
@@ -11,49 +11,41 @@
 #include <SFML/Graphics.h>
 #include "plane.h"
 
-plane_t *plane_create(sfVector2f departure, sfVector2f arrival,
-                    unsigned int speed, unsigned int delay)
+plane_t *plane_create(path_t *path, sfTexture *texture, unsigned int delay)
 {
-    plane_t *plane   = malloc(sizeof(*plane));
+    plane_t *plane = malloc(sizeof(*plane));
 
-    plane->pos       = departure;
-    plane->end       = arrival;
-    plane->speed     = speed;
-    plane->delay     = delay;
-    plane->step      = get_step_offset(departure, arrival);
-    plane->step.x   *= speed;
-    plane->step.y   *= speed;
-    plane->texture   = sfTexture_createFromFile(PLANE_TEXTURE_PATH, NULL);
-    plane->sprite    = sfSprite_create();
-    sfSprite_setTexture(plane->sprite, plane->texture, sfTrue);
-    sfSprite_setPosition(plane->sprite, plane->pos);
+    if (!plane)
+        return (NULL);
+    plane->path   = path;
+    plane->delay  = delay;
+    plane->sprite = sfSprite_create();
+    sfSprite_setTexture(plane->sprite, texture, sfTrue);
+    sfSprite_setPosition(plane->sprite, plane->path->pos);
     sfSprite_rotate(plane->sprite,
-                    get_angle_from_coordinate(arrival, departure));
+                get_angle_from_coordinate(plane->path->end, plane->path->pos));
     return (plane);
+}
+
+void plane_destroy(plane_t *plane)
+{
+    if (plane->path)
+        free(plane->path);
+    if (plane->sprite)
+        sfSprite_destroy(plane->sprite);
+    if (plane)
+        free(plane);
 }
 
 void plane_update_pos(plane_t *plane)
 {
-    unsigned int x_diff = abs((int)(plane->end.x) - (int)(plane->pos.x));
-    unsigned int y_diff = abs((int)(plane->end.y) - (int)(plane->pos.y));
-
-    if (x_diff > 1 && y_diff > 1) {
-        plane->pos.x += plane->step.x;
-        plane->pos.y += plane->step.y;
-        sfSprite_move(plane->sprite, plane->step);
+    if (plane->path->diff.x > 1 && plane->path->diff.y > 1) {
+        plane->path->pos.x  += plane->path->step.x;
+        plane->path->pos.y  += plane->path->step.y;
+        plane->path->diff.x += plane->path->step.x;
+        plane->path->diff.y += plane->path->step.y;
+        sfSprite_move(plane->sprite, plane->path->step);
     }
-}
-
-sfVector2f get_step_offset(sfVector2f point_a, sfVector2f point_b)
-{
-    int dx = point_b.x - point_a.x;
-    int dy = point_b.y - point_a.y;
-    unsigned int steps = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
-    sfVector2f offset;
-
-    offset.x = dx / (float)steps;
-    offset.y = dy / (float)steps;
-    return (offset);
 }
 
 float get_angle_from_coordinate(sfVector2f point_a, sfVector2f point_b)
