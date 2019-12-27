1ea1644b68ec9d677b9a12461bd335888c86f4f6
Julien Pause <julien.pause@epitech.eu>
2019-12-27 18:12:50 +0400

    Update file_manipulation header file function prototypes

--- include/file_manipulation.h
+++ include/file_manipulation.h
@@ -9,6 +9,8 @@
     #define FILE_MANIPULATION_H_
 
     #include <stdio.h>
+    #include <SFML/Graphics.h>
+    #include "sim.h"
     #include "plane.h"
     #include "tower.h"
 
@@ -18,10 +20,8 @@
     char *get_file_buffer(char const *file_path);
     unsigned int get_file_size(char const *file_path);
 
-    int get_entities_from_file(char const *file_path, plane_t ***, tower_t ***);
-    unsigned int get_nb_entities_from_file(char const *file_path,
-                                            char const symbol);
-    int fill_entities_by_line(FILE *stream, plane_t ***, tower_t ***);
-    plane_t *add_plane_from_line(char *current_line);
-    tower_t *add_tower_from_line(char *current_line);
+    int get_entities_from_file(char const *file_path, sim_t *sim);
+    int fill_entities_by_line(FILE *stream, sim_t *sim);
+    plane_t *add_plane_from_line(char *current_line, sfTexture *);
+    tower_t *add_tower_from_line(char *current_line, sfTexture *);
 #endif
