83f08d6e9831232bb99f9ca67a1faa138daa837d
Julien Pause <julien.pause@epitech.eu>
2019-12-28 11:35:43 +0400

    Remove debugging printf in pause_sim

--- src/events.c
+++ src/events.c
@@ -35,11 +35,10 @@ void pause_sim(sim_t *sim)
     sfInt32 c_time = sfTime_asMilliseconds(sfClock_getElapsedTime(sim->clock));
     sfInt32 delay = c_time + 500;
 
-    printf("You pressed it !\n");
     if (sim->is_paused) {
         sim->is_paused = sfFalse;
     } else {
-        draw_pause_menu(sim->window->window);
+        draw_pause_menu(sim->window);
         sim->is_paused = sfTrue;
     }
     while (c_time < delay)
