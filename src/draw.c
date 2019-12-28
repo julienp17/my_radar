b9d28cd76f333aba1f6598b4a8b15b2098e8b1bc
Julien Pause <julien.pause@epitech.eu>
2019-12-28 21:45:11 +0400

    Remove draw_planes and create draw_timer function in draw.c

--- src/draw.c
+++ src/draw.c
@@ -9,22 +9,13 @@
 #include "window.h"
 #include "tower.h"
 #include "plane.h"
+#include "my.h"
 
 void draw_towers(sfRenderWindow *window, tower_t **towers)
 {
     for (unsigned int i = 0 ; towers[i] ; i++) {
         sfRenderWindow_drawSprite(window, towers[i]->sprite, NULL);
         sfRenderWindow_drawCircleShape(window, towers[i]->control_area, NULL);
-    }
-}
-
-void draw_planes(sfRenderWindow *window, plane_t **planes, sfClock *clock)
-{
-    for (unsigned int i = 0 ; planes[i] ; i++) {
-        if (sfTime_asSeconds(sfClock_getElapsedTime(clock)) > planes[i]->delay){
-            plane_update_pos(planes[i]);
-            sfRenderWindow_drawRectangleShape(window, planes[i]->hitbox, NULL);
-        }
     }
 }
 
@@ -39,4 +30,21 @@ void draw_pause_menu(window_t *window)
     sfRenderWindow_drawRectangleShape(window->window, pause_menu, NULL);
     sfRenderWindow_display(window->window);
     sfRectangleShape_destroy(pause_menu);
+}
+
+void draw_timer(window_t *window, sfClock *clock)
+{
+    sfInt32 time = sfTime_asSeconds(sfClock_getElapsedTime(clock));
+    sfText *str_time = sfText_create();
+    sfVector2f pos = {window->width - 100, 0};
+    sfFont *font = sfFont_createFromFile("assets/fonts/skyfont.otf");
+
+    sfText_setCharacterSize(str_time, 100);
+    sfText_setFont(str_time, font);
+    sfText_setColor(str_time, sfBlack);
+    sfText_setString(str_time, my_int_to_strnum(time));
+    sfText_setPosition(str_time, pos);
+    sfRenderWindow_drawText(window->window, str_time, NULL);
+    sfText_destroy(str_time);
+    sfFont_destroy(font);
 }
