60631c99d8cecda91cd96cf9b10cbb675ff1fb15
Julien Pause <julien.pause@epitech.eu>
2019-12-28 11:12:29 +0400

    Refactor my_radar to only update and draw if simulation is not paused

--- src/my_radar.c
+++ src/my_radar.c
@@ -9,9 +9,9 @@
 #include <SFML/Graphics.h>
 #include "usage.h"
 #include "my_radar.h"
-#include "draw.h"
-#include "file_manipulation.h"
 #include "sim.h"
+#include "draw.h"
+#include "events.h"
 
 int my_radar(char const *script_path)
 {
@@ -20,10 +20,12 @@ int my_radar(char const *script_path)
     if (sim == NULL)
         return (MY_EXIT_FAILURE);
     while (sfRenderWindow_isOpen(sim->window->window)) {
-        window_poll_quit(sim->window->window);
-        sfRenderWindow_clear(sim->window->window, sfWhite);
-        simulation_loop(sim);
-        sfRenderWindow_display(sim->window->window);
+        poll_events(sim);
+        if (!(sim->is_paused)) {
+            sfRenderWindow_clear(sim->window->window, sfWhite);
+            simulation_loop(sim);
+            sfRenderWindow_display(sim->window->window);
+        }
     }
     sim_destroy(sim);
     return (MY_EXIT_SUCCESS);
