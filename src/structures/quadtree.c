cec1afd52d32a56f6a708bd5aa272af081a80a5f
Julien Pause <julien.pause@epitech.eu>
2019-12-29 00:07:05 +0400

    Remove quadtree_destroy and refactor quadtree.c

--- src/structures/quadtree.c
+++ src/structures/quadtree.c
@@ -27,28 +27,17 @@ quadtree_t *quadtree_create(sfIntRect boundary)
     return (quadtree);
 }
 
-void quadtree_destroy(quadtree_t *quadtree)
-{
-    for (unsigned int i = 0 ; quadtree->planes[i] ; i++)
-        plane_destroy(quadtree->planes[i]);
-    if (quadtree->is_divided)
-        for (unsigned int i = 0 ; i < 4 ; i++)
-            quadtree_destroy(quadtree->children[i]);
-    if (quadtree)
-        free(quadtree);
-}
-
 void quadtree_clear(quadtree_t *quadtree)
 {
     if (quadtree->is_divided) {
         for (unsigned int i = 0 ; quadtree->children[i] ; i++) {
             quadtree_clear(quadtree->children[i]);
-            quadtree_destroy(quadtree->children[i]);
+            free(quadtree->children[i]);
         }
         quadtree->is_divided = sfFalse;
     }
     for (unsigned int i = 0 ; quadtree->planes[i] ; i++)
-        plane_destroy(quadtree->planes[i]);
+        quadtree->planes[i] = NULL;
     quadtree->nb_planes = 0;
 }
 
@@ -62,9 +51,14 @@ int quadtree_insert(quadtree_t *quadtree, plane_t *plane)
         quadtree->nb_planes++;
         return (0);
     }
-    if (!(quadtree->is_divided))
+    if (!(quadtree->is_divided)) {
         if (quadtree_subdivide(&quadtree) != 0)
             return (-1);
+        for (unsigned int i = 0 ; i < QT_CAPACITY ; i++) {
+            quadtree_insert(quadtree, quadtree->planes[i]);
+            quadtree->planes[i] = NULL;
+        }
+    }
     for (unsigned int i = 0 ; i < 4 ; i++)
         if (quadtree_insert(quadtree->children[i], plane) == 0) {
             quadtree->nb_planes++;
@@ -84,7 +78,7 @@ int quadtree_subdivide(quadtree_t **quadtree)
     if (!((*quadtree)->children))
         return (1);
     (*quadtree)->children[0] = quadtree_create((sfIntRect){x, y, w / 2, h / 2});
-    (*quadtree)->children[1] = quadtree_create((sfIntRect){x + w / 2, y + h /2,
+    (*quadtree)->children[1] = quadtree_create((sfIntRect){x + w / 2, y,
                                                 w / 2, h / 2});
     (*quadtree)->children[2] = quadtree_create((sfIntRect){x, y + h / 2,
                                                 w / 2, h / 2});
