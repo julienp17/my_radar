a3f5f0d2301a54bd445262b69234112e55802d08
Julien Pause <julien.pause@epitech.eu>
2019-12-29 18:57:34 +0400

    Refactor plane.c entirely to work with axis aligned bounding boxes

--- src/structures/plane.c
+++ src/structures/plane.c
@@ -11,24 +11,42 @@
 #include "plane.h"
 #include "utils.h"
 
+static plane_t *plane_init(plane_t *plane, path_t *path, unsigned int delay);
+
 plane_t *plane_create(path_t *path, sfTexture *texture, unsigned int delay)
 {
     plane_t *plane = malloc(sizeof(*plane));
 
     if (!plane)
         return (NULL);
-    plane->path   = path;
-    plane->delay  = delay;
-    plane->hitbox = sfRectangleShape_create();
-    if (!(plane->hitbox))
+    plane->sprite = sfSprite_create();
+    plane->outline = sfRectangleShape_create();
+    if (!(plane->sprite) || !(plane->outline))
         return (NULL);
-    sfRectangleShape_setSize(plane->hitbox, (sfVector2f) {20.0, 20.0});
-    sfRectangleShape_setOutlineColor(plane->hitbox, sfYellow);
-    sfRectangleShape_setOutlineThickness(plane->hitbox, 2.0);
-    sfRectangleShape_setTexture(plane->hitbox, texture, sfTrue);
-    sfRectangleShape_setPosition(plane->hitbox, plane->path->pos);
-    sfRectangleShape_rotate(plane->hitbox,
-                get_angle_from_coordinate(plane->path->end, plane->path->pos));
+    sfSprite_setTexture(plane->sprite, texture, sfTrue);
+    sfSprite_setOrigin(plane->sprite, (sfVector2f) {10, 10});
+    sfRectangleShape_setFillColor(plane->outline, sfTransparent);
+    sfRectangleShape_setOutlineColor(plane->outline, sfYellow);
+    sfRectangleShape_setOutlineThickness(plane->outline, 2.0);
+    return (plane_init(plane, path, delay));
+}
+
+static plane_t *plane_init(plane_t *plane, path_t *path, unsigned int delay)
+{
+    plane->delay = delay;
+    plane->path = path;
+    plane->angle = get_angle_from_coordinate(plane->path->end,plane->path->pos);
+    sfSprite_setPosition(plane->sprite, plane->path->pos);
+    sfSprite_setRotation(plane->sprite, 0.0);
+    sfSprite_move(plane->sprite, (sfVector2f) {10, 10});
+    sfSprite_rotate(plane->sprite, plane->angle);
+    plane->hitbox = (sfIntRect) {(int)plane->path->pos.x,
+                                (int)plane->path->pos.y, 20,20};
+    plane->hitbox = get_bounding_box_of_rotated(plane->hitbox, plane->angle);
+    sfRectangleShape_setPosition(plane->outline, (sfVector2f)
+                                {plane->hitbox.left, plane->hitbox.top});
+    sfRectangleShape_setSize(plane->outline, (sfVector2f)
+                                {plane->hitbox.width, plane->hitbox.height});
     return (plane);
 }
 
@@ -36,8 +54,10 @@ void plane_destroy(plane_t *plane)
 {
     if (plane->path)
         free(plane->path);
-    if (plane->hitbox)
-        sfRectangleShape_destroy(plane->hitbox);
+    if (plane->sprite)
+        sfSprite_destroy(plane->sprite);
+    if (plane->outline)
+        sfRectangleShape_destroy(plane->outline);
     if (plane)
         free(plane);
 }
@@ -46,26 +66,20 @@ void plane_move(plane_t *plane, sfVector2f const offset)
 {
     plane->path->pos.x  += offset.x;
     plane->path->pos.y  += offset.y;
-    sfRectangleShape_move(plane->hitbox, offset);
+    plane->hitbox.left  += offset.x;
+    plane->hitbox.top += offset.y;
+    sfSprite_move(plane->sprite, offset);
+    sfRectangleShape_move(plane->outline, offset);
 }
 
 void plane_reset_random(plane_t *plane, tower_t **towers, sfClock *clock)
 {
-    sfInt32 c_time = sfTime_asSeconds(sfClock_getElapsedTime(clock));
-    sfVector2f new_beg;
-    sfVector2f new_end;
-    unsigned int new_speed = rand() % (6 + 1 - 3) + 3;
+    path_t *new_path = NULL;
+    unsigned int new_delay = 0;
 
     if (plane->path)
         free(plane->path);
-    new_beg = get_random_tower_pos(towers);
-    new_end = get_random_tower_pos(towers);
-    while (pos_match(new_beg, new_end))
-        new_end = get_random_tower_pos(towers);
-    plane->delay = c_time + rand() % 10;
-    plane->path = path_create(new_beg, new_end, new_speed);
-    sfRectangleShape_setPosition(plane->hitbox, plane->path->pos);
-    sfRectangleShape_setRotation(plane->hitbox, 0.0);
-    sfRectangleShape_rotate(plane->hitbox,
-                get_angle_from_coordinate(plane->path->end, plane->path->pos));
+    new_path = get_random_path(towers);
+    new_delay = sfTime_asSeconds(sfClock_getElapsedTime(clock)) + rand() % 10;
+    plane = plane_init(plane, new_path, new_delay);
 }
