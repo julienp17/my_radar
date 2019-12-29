056a25a77bee363d37a9a27d59d2a4647f8964e3
Julien Pause <julien.pause@epitech.eu>
2019-12-29 13:07:11 +0400

    Add new boundary boolean functions prototypes in utils.h

--- include/utils.h
+++ include/utils.h
@@ -13,4 +13,6 @@
     float get_angle_from_coordinate(sfVector2f point_a, sfVector2f point_b);
     sfBool pos_match(sfVector2f, sfVector2f);
     sfBool pos_are_near(sfVector2f, sfVector2f, float const threshold);
+    sfBool boundary_contains_pos(sfIntRect const boundary,sfVector2f const pos);
+    sfBool boundary_overlaps(sfIntRect const boundary, sfIntRect const other);
 #endif
