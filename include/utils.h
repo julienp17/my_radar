63d7320ebb4215f2658f89412b8a63b4f2a1b146
Julien Pause <julien.pause@epitech.eu>
2019-12-29 19:00:11 +0400

    Add get_rotated_point, get_rotated_points, and get_bounding_box_of_rotated functions prototypes in util.h

--- include/utils.h
+++ include/utils.h
@@ -11,6 +11,12 @@
     #include <SFML/Graphics.h>
 
     float get_angle_from_coordinate(sfVector2f point_a, sfVector2f point_b);
+
+    sfVector2i get_rotated_point(sfVector2i point, sfVector2i center,
+                                float angle);
+    sfVector2i *get_rotated_points(sfIntRect hitbox, float angle);
+    sfIntRect get_bounding_box_of_rotated(sfIntRect const hitbox, float angle);
+
     sfBool pos_match(sfVector2f, sfVector2f);
     sfBool pos_are_near(sfVector2f, sfVector2f, float const threshold);
     sfBool boundary_contains_pos(sfIntRect const boundary,sfVector2f const pos);
