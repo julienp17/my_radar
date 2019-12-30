ae8cf00a00a964df76733217807d8ad69cd7178b
Julien Pause <julien.pause@epitech.eu>
2019-12-31 00:10:42 +0400

    Call menu in main

--- src/main.c
+++ src/main.c
@@ -9,17 +9,20 @@
 #include <time.h>
 #include "my_radar.h"
 #include "usage.h"
+#include "start_menu.h"
 
 int main(int ac, char **av)
 {
-    int error_code = 0;
+    int exit_code = 0;
 
-    error_code = check_args(ac, av);
-    if (error_code == MY_EXIT_OPTION)
+    exit_code = check_args(ac, av);
+    if (exit_code == MY_EXIT_OPTION)
         return (MY_EXIT_SUCCESS);
-    else if (error_code == MY_EXIT_FAILURE)
-        return (error_code);
+    else if (exit_code == MY_EXIT_FAILURE)
+        return (exit_code);
     srand(time(NULL));
-    error_code = my_radar(av[1]);
-    return (error_code);
+    exit_code = start_menu();
+    if (exit_code == 1)
+        exit_code = my_radar(av[1]);
+    return (exit_code);
 }
