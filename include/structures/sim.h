2b7034c7b0d24094966dbfc7b8c52e03c29ef0fc
Julien Pause <julien.pause@epitech.eu>
2019-12-31 18:07:39 +0400

    Remove plane_texture and tower_texture attributes of sim, replaced by sim_texture_t *textures

--- include/structures/sim.h
+++ include/structures/sim.h
@@ -13,18 +13,14 @@
     #include "quadtree.h"
     #include "plane.h"
     #include "tower.h"
-
-    #define SIM_BG_IMG_PATH        "assets/backgrounds/1440p/world_map_1440.png"
-    #define SIM_PLANE_TEXTURE_PATH "assets/textures/plane2_small.png"
-    #define SIM_TOWER_TEXTURE_PATH "assets/textures/tower_small.png"
+    #include "sim_textures.h"
 
     typedef struct simulation {
         window_t *window;
         quadtree_t *quadtree;
         plane_t **planes;
         tower_t **towers;
-        sfTexture *plane_texture;
-        sfTexture *tower_texture;
+        sim_textures_t *textures;
         sfClock *clock;
         sfBool is_paused;
     } sim_t;
