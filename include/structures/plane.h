7409d162369feaf1e3a215600b15e0452e53e05f
Julien Pause <julien.pause@epitech.eu>
2019-12-31 13:49:39 +0400

    Update plane structures functions prototype and add new macro PLANE_SPRITE_SIZE set to 20

--- include/structures/plane.h
+++ include/structures/plane.h
@@ -11,8 +11,10 @@
     #include <SFML/Graphics.h>
     #include "tower.h"
     #include "path.h"
+    #include "utils.h"
 
     #define PLANE_SYMBOL            'A'
+    #define PLANE_SPRITE_SIZE       20.0
 
     typedef struct plane {
         path_t *path;
@@ -23,8 +25,11 @@
         sfRectangleShape *outline;
     } plane_t;
 
-    plane_t *plane_create(path_t *path, sfTexture *texture, unsigned int delay);
+    plane_t *plane_create(path_t *path, sfTexture *texture, uint delay,
+                            uint w_width);
+    plane_t *plane_init(plane_t *plane, path_t *path, uint delay, uint w_width);
     void plane_destroy(plane_t *plane);
-    void plane_move(plane_t *plane, sfVector2f const offset);
-    void plane_reset_random(plane_t *, tower_t **towers, unsigned int c_time);
+    void plane_move(plane_t *plane, sfVector2f const offset, uint w_width);
+    void plane_reset_random(plane_t *plane, tower_t **towers,
+                            uint c_time, uint w_width);
 #endif
