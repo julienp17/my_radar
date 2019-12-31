4552e8495b653bdfb148cac19b1cd866e8dcdae1
Julien Pause <julien.pause@epitech.eu>
2019-12-31 17:02:01 +0400

    Move geometry.c to utilities/

--- src/utilities/geometry.c
+++ src/utilities/geometry.c
@@ -35,33 +35,6 @@ sfVector2f get_rotated_point(sfVector2f point, sfVector2f center, float angle)
     return (rotated_point);
 }
 
-sfVector2f *get_corners(sfFloatRect const rect)
-{
-    sfVector2f *corners = malloc(sizeof(sfVector2f) * 4);
-
-    if (!corners)
-        return (NULL);
-    corners[0] = (sfVector2f) {rect.left, rect.top};
-    corners[1] = (sfVector2f) {rect.left + rect.width, rect.top};
-    corners[2] = (sfVector2f) {rect.left, rect.top + rect.height};
-    corners[3] = (sfVector2f) {rect.left + rect.width,
-                                rect.top + rect.height};
-    return (corners);
-}
-
-sfVector2f *get_rotated_corners(sfFloatRect hitbox, float angle)
-{
-    sfVector2f center = {hitbox.left + hitbox.width / 2.0,
-                        hitbox.top + hitbox.height / 2.0};
-    sfVector2f *corners = get_corners(hitbox);
-
-    if (!corners)
-        return (NULL);
-    for (unsigned int i = 0 ; i < 4 ; i++)
-        corners[i] = get_rotated_point(corners[i], center, angle);
-    return (corners);
-}
-
 sfFloatRect get_bounding_box_of_rotated(sfFloatRect const hitbox, float angle)
 {
     sfVector2f *rotated_corners = get_rotated_corners(hitbox, angle);
