8fc3ff9d64f4c21d32e3d2cf7afda5069b0de230
Julien Pause <julien.pause@epitech.eu>
2020-01-02 09:45:20 +0400

    Add sfEvtKeyReleased event to pause_sim condition

--- src/events/poll_events.c
+++ src/events/poll_events.c
@@ -17,7 +17,7 @@ void sim_poll_events(sim_t *sim)
 
     while (sfRenderWindow_pollEvent(sim->window->render, &event)) {
         check_window_quit(&event, sim->window->render);
-        if (event.key.code == sfKeySpace)
+        if (event.type == sfEvtKeyReleased && event.key.code == sfKeySpace)
             pause_sim(sim);
     }
 }
