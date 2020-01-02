79704467d248b8ad60fe43c4f616d38cd0359369
Julien Pause <julien.pause@epitech.eu>
2020-01-02 22:33:23 +0400

    Add sim_states structure

---
+++ src/structures/sim/sim_states.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2020
+** my_radar
+** File description:
+** Source file for structure sim_states
+*/
+
+#include <stdlib.h>
+#include <SFML/Graphics.h>
+#include "sim_states.h"
+
+sim_states_t *sim_states_create(void)
+{
+    sim_states_t *states = malloc(sizeof(sim_states_t));
+
+    if (!states)
+        return (NULL);
+    states->show_hitbox = sfTrue;
+    states->show_sprites = sfTrue;
+    return (states);
+}
+
+void sim_states_destroy(sim_states_t *states)
+{
+    if (states)
+        free(states);
+}
