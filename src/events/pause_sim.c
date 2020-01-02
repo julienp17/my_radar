e8cc594d15067dd1ca049841c855b8d4820cc441
Julien Pause <julien.pause@epitech.eu>
2020-01-02 09:49:57 +0400

    Refactor pause_sim to not wait a delay

--- src/events/pause_sim.c
+++ src/events/pause_sim.c
@@ -11,15 +11,10 @@
 
 void pause_sim(sim_t *sim)
 {
-    sfInt32 c_time = sfTime_asMilliseconds(sfClock_getElapsedTime(sim->clock));
-    sfInt32 delay = c_time + 500;
-
     if (sim->is_paused) {
         sim->is_paused = sfFalse;
     } else {
         draw_pause_menu(sim->window);
         sim->is_paused = sfTrue;
     }
-    while (c_time < delay)
-        c_time = sfTime_asMilliseconds(sfClock_getElapsedTime(sim->clock));
 }
