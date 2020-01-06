a8e83ab6e4be4f0579f071ff48d08dcd5d4f86f8
Julien Pause <julien.pause@epitech.eu>
2020-01-06 10:39:53 +0400

    Remove start_menu from simulation

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@
 
 int main(int ac, char **av)
 {
+    sim_t *sim = NULL;
     int error_code = 0;
 
     error_code = check_args(ac, av);
@@ -23,32 +24,12 @@ int main(int ac, char **av)
     else if (error_code == MY_EXIT_FAILURE)
         return (error_code);
     srand(time(NULL));
-    error_code = my_radar(av[1]);
-    return (error_code);
-}
-
-int my_radar(char const *script_path)
-{
-    sim_t *sim = sim_create_from_script(script_path);
-
+    sim = sim_create_from_script(av[1]);
     if (!sim) {
         my_puterr("Simulation couldn't be created.\n");
         return (MY_EXIT_FAILURE);
     }
-    window_set_background(sim->window, sim->textures->start_menu_bg);
-    if (start_menu(sim->window->render, sim->window->background) == 1)
-        launch_simulation(sim);
+    launch_simulation(sim);
     sim_destroy(sim);
-    return (MY_EXIT_SUCCESS);
-}
-
-int start_menu(sfRenderWindow *render, sfSprite *background)
-{
-    int exit_code = 0;
-
-    sfRenderWindow_drawSprite(render, background, NULL);
-    sfRenderWindow_display(render);
-    while (sfRenderWindow_isOpen(render) && exit_code == 0)
-        start_menu_poll_events(render, &exit_code);
-    return (exit_code);
+    return (error_code);
 }
