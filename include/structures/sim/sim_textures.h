5e23b63f9be9f8b81621e2c4706f714876b1d7a6
Julien Pause <julien.pause@epitech.eu>
2020-01-06 11:19:29 +0400

    Remove start_menu_bg_texture from sim_textures

--- include/structures/sim/sim_textures.h
+++ include/structures/sim/sim_textures.h
@@ -10,7 +10,7 @@
 
     #include <SFML/Graphics.h>
 
-    #define START_MENU_BG_TEXTURE_PATH "assets/backgrounds/start_menu.png"
+    #define S_MENU_BG_TEXTURE_PATH     "assets/backgrounds/start_menu.png"
     #define SIM_BG_TEXTURE_PATH        "assets/backgrounds/world_map.png"
     #define PLANE_TEXTURE_PATH         "assets/textures/plane.png"
     #define TOWER_TEXTURE_PATH         "assets/textures/tower.png"
@@ -18,7 +18,6 @@
     typedef sfTexture texture_t;
 
     typedef struct sim_textures {
-        texture_t *start_menu_bg;
         texture_t *sim_bg;
         texture_t *plane;
         texture_t *tower;
--- src/structures/sim/sim_textures.c
+++ src/structures/sim/sim_textures.c
@@ -18,11 +18,7 @@ sim_textures_t *sim_textures_create(void)
     textures->plane = sfTexture_createFromFile(PLANE_TEXTURE_PATH, NULL);
     textures->tower = sfTexture_createFromFile(TOWER_TEXTURE_PATH, NULL);
     textures->sim_bg = sfTexture_createFromFile(SIM_BG_TEXTURE_PATH, NULL);
-    textures->start_menu_bg = sfTexture_createFromFile(
-                                START_MENU_BG_TEXTURE_PATH, NULL);
-    if (!(textures->plane) || !(textures->tower))
-        return (NULL);
-    if (!(textures->sim_bg) || !(textures->start_menu_bg))
+    if (!(textures->sim_bg) || !(textures->plane) || !(textures->tower))
         return (NULL);
     return (textures);
 }
@@ -35,8 +31,6 @@ void sim_textures_destroy(sim_textures_t *textures)
         sfTexture_destroy(textures->tower);
     if (textures->sim_bg)
         sfTexture_destroy(textures->sim_bg);
-    if (textures->start_menu_bg)
-        sfTexture_destroy(textures->start_menu_bg);
     if (textures)
         free(textures);
 }
