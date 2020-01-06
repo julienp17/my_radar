52f9e1064b384a34659ac6bf3b91ecb06a56d2e7
Julien Pause <julien.pause@epitech.eu>
2020-01-06 15:03:07 +0400

    Add graphical header file

---
+++ bonus/include/structures/graphical.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** my_radar
+** File description:
+** Header file for struct graphical
+*/
+
+#ifndef GRAPHICAL_H_
+    #define GRAPHICAL_H_
+
+    #include "window.h"
+    #include "sim_fonts.h"
+    #include "sim_texts.h"
+    #include "sim_textures.h"
+
+    typedef struct graphical {
+        window_t *window;
+        textures_t *textures;
+        fonts_t *fonts;
+        texts_t *texts;
+    } graphical_t;
+    typedef graphical_t gl_t;
+
+    gl_t *gl_create(window_t *window);
+    void gl_destroy(gl_t *gl);
+#endif
