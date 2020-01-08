1f2dc9e5cd43b9e937fe95e3bf44efbcd9a7ae5e
Julien Pause <julien.pause@epitech.eu>
2020-01-08 22:33:30 +0400

    Add draw_info.c

---
+++ bonus/src/draw/draw_info.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** my_radar
+** File description:
+** Draws info about the info
+*/
+
+#include <SFML/Graphics.h>
+#include "sim_info.h"
+#include "sim_texts.h"
+#include "sim_states.h"
+
+void draw_info(sfRenderWindow *render, info_t *info, texts_t *texts,
+                states_t *states)
+{
+    if (!(states->show_info))
+        return;
+    sfRenderWindow_drawText(render, texts->nb_planes, NULL);
+    sfRenderWindow_drawText(render, texts->nb_towers, NULL);
+    (void)info;
+}
