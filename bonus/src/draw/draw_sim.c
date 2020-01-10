b4c0fe4c94fa022e6498eae4ae0301c12bd50269
Julien Pause <julien.pause@epitech.eu>
2020-01-10 23:13:18 +0400

    Add draw_info in draw_sim

--- bonus/src/draw/draw_sim.c
+++ bonus/src/draw/draw_sim.c
@@ -12,10 +12,11 @@
 void draw_sim(sim_t *sim)
 {
     draw_background(sim->gl->window, sim->gl->window->background,
-                    sim->gl->sprites->timelapse);
+                    sim->gl->timelapse);
     draw_towers(sim->gl->window->render, sim->towers, sim->state);
     draw_quadtree(sim->gl->window->render, sim->quadtree, sim->state);
     for (unsigned int i = 0 ; sim->planes[i] ; i++)
         plane_loop(sim->planes[i], sim, sim->info->timer);
     draw_timer(sim->gl->window->render, sim->gl->texts->timer,sim->info->timer);
+    draw_info(sim->gl->window->render, sim->info, sim->gl->texts, sim->state);
 }
